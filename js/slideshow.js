229e10f6a30f60a857b87e3b21e0a7354d8d6762
Mike Quigley <mdq204@gmail.com>
2020-03-04 11:08:39 -0500

    Remove onload from slideshow script

--- js/slideshow.js
+++ js/slideshow.js
@@ -30,7 +30,3 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-
-window.onload = function () {
-    currentSlide(10);
-}
